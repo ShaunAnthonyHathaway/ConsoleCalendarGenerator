080e5e7ef92c028340112a2feb4379eacd6ee060
Shaun Hathaway <Shaun.Hathaway@outlook.com>
2023-06-24 18:57:38 -0700

    added comments

--- ConsoleCalendarGeneratorConsole/Program.cs
+++ ConsoleCalendarGeneratorConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿var CalendarExample1 = new CalendarGenerator.ConsoleCalendar();
+﻿//generate a month at a time with the selected dates passed to each month
+var CalendarExample1 = new CalendarGenerator.ConsoleCalendar();
 CalendarExample1.GenerateMonth(1, 2023, new List<DateTime>() { new DateTime(2023, 1, 1) });
 CalendarExample1.SetTitleColor(ConsoleColor.Red);
 CalendarExample1.GenerateMonth(2, 2023, new List<DateTime>() { new DateTime(2023, 2, 1), new DateTime(2023, 2, 4) });
@@ -9,7 +10,7 @@ CalendarExample1.GenerateMonth(4, 2023, new List<DateTime>() { new DateTime(2023
 CalendarExample1.SetDateColor(ConsoleColor.DarkYellow);
 CalendarExample1.GenerateMonth(5, 2023, new List<DateTime>() { new DateTime(2023, 5, 1), new DateTime(2023, 5, 8), new DateTime(2023, 5, 11) });
 Pause();
-
+//generate a month at a time with a selected date list
 var CalendarExample2 = new CalendarGenerator.ConsoleCalendar(ConsoleColor.Red);
 List<DateTime> HitDates = new List<DateTime>()
 {
@@ -28,12 +29,12 @@ CalendarExample2.GenerateMonth(3, 2023, HitDates);
 CalendarExample2.GenerateMonth(4, 2023, HitDates);
 CalendarExample2.GenerateMonth(5, 2023, HitDates);
 Pause();
-
-CalendarExample2.SetTitleColor(ConsoleColor.DarkMagenta);
+//Generate a year with no selected dates
+CalendarExample2.SetTitleColor(ConsoleColor.DarkMagenta);//Change title color
 CalendarExample2.GenerateYear(2023);
 Pause();
-
-var CalendarExample4 = new CalendarGenerator.ConsoleCalendar(ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Magenta, ConsoleColor.Blue);
+//Generate a year with selected dates passed
+var CalendarExample4 = new CalendarGenerator.ConsoleCalendar(ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Magenta, ConsoleColor.Blue);//set all colors on instantiation
 CalendarExample4.GenerateYear(2023, HitDates);
 Console.WriteLine("\n\nPress any key to terminate...");
 Console.ReadKey();

93f2d5d32537676b01806b2b6995a5628bd5c7dc
Shaun Hathaway <Shaun.Hathaway@outlook.com>
2023-06-24 18:52:32 -0700

    added library and examples

---
+++ ConsoleCalendarGenerator/ConsoleCalendar.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CalendarGenerator
+{
+    public class ConsoleCalendar
+    {
+        private ConsoleColor _dayOfWeekColor { get; set; }
+        private ConsoleColor _dateColor { get; set; }
+        private ConsoleColor _selectedDateColor { get; set; }
+        private ConsoleColor _titleColor { get; set; }
+        public void GenerateYear(int Year)
+        {
+            GenerateYear(Year, new List<DateTime>());
+        }
+        public void GenerateYear(int Year, List<DateTime> dates)
+        {
+            int MonthCounter = 0;
+            while (MonthCounter < 12)
+            {
+                MonthCounter++;
+                GenerateMonth(MonthCounter, Year, dates);
+            }
+        }
+        public void GenerateMonth(int Month, int Year)
+        {
+            GenerateMonth(Month, Year, new List<DateTime>());
+        }
+        public void GenerateMonth(int Month, int Year, List<DateTime> dates)
+        {
+            List<Int32> HitDays = new List<Int32>();
+            foreach (DateTime dt in dates)
+            {
+                if (dt.Month == Month && dt.Year == Year && HitDays.Contains(dt.Day) == false)
+                {
+                    HitDays.Add(dt.Day);
+                }
+            }
+            int days = DateTime.DaysInMonth(Year, Month);
+            String StartDay = ConvertToShortDay(new DateTime(Year, Month, 1).DayOfWeek);
+            Console.ForegroundColor = _titleColor;
+            String MonthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(Month);
+            Console.WriteLine(GetSpaces(MonthName) + MonthName + " " + Year);
+            Console.ResetColor();
+            Console.ForegroundColor = _dayOfWeekColor;
+            Console.WriteLine("Mo Tu We Th Fr Sa Su");
+            Console.ResetColor();
+            int Counter = 1;
+            bool Started = false;
+            String ThisDayOfWeek = "Mo";
+            days++;
+            while (Counter < days)
+            {
+                if (!Started)
+                {
+                    if (ThisDayOfWeek == StartDay)
+                    {
+                        Started = true;
+                        SetColor(Counter, ref HitDays);
+                        Console.Write(" " + Counter);
+                        Counter++;
+                    }
+                    else
+                    {
+                        SetColor(Counter, ref HitDays);
+                        Console.Write("   ");
+                    }
+                }
+                else
+                {
+                    if (Counter < 10)
+                    {
+                        if (ThisDayOfWeek == "Mo")
+                        {
+                            SetColor(Counter, ref HitDays);
+                            Console.Write(" " + Counter);
+                        }
+                        else
+                        {
+                            SetColor(Counter, ref HitDays);
+                            Console.Write("  " + Counter);
+                        }
+                    }
+                    else
+                    {
+                        if (ThisDayOfWeek == "Mo")
+                        {
+                            SetColor(Counter, ref HitDays);
+                            Console.Write(Counter);
+                        }
+                        else
+                        {
+                            SetColor(Counter, ref HitDays);
+                            Console.Write(" " + Counter);
+                        }
+                    }
+                    Counter++;
+                }
+                if (ThisDayOfWeek == "Su")
+                {
+                    Console.WriteLine();
+                }
+                if (Counter == days)
+                {
+                    Console.Write("\n");
+                    Console.WriteLine();
+                }
+                ThisDayOfWeek = GetNextShortDay(ThisDayOfWeek);
+            }
+            Console.ResetColor();
+        }
+        public ConsoleColor GetDayOfWeekColor()
+        {
+            return _dayOfWeekColor;
+        }
+        public void SetDayOfWeekColor(ConsoleColor TitleColor)
+        {
+            _dayOfWeekColor = TitleColor;
+        }
+        public ConsoleColor GetDateColor()
+        {
+            return _dateColor;
+        }
+        public void SetDateColor(ConsoleColor TitleColor)
+        {
+            _dateColor = TitleColor;
+        }
+        public ConsoleColor GetSelectedDateColor()
+        {
+            return _selectedDateColor;
+        }
+        public void SetSelectedDateColor(ConsoleColor TitleColor)
+        {
+            _selectedDateColor = TitleColor;
+        }
+        public ConsoleColor GetTitleColor()
+        {
+            return _titleColor;
+        }
+        public void SetTitleColor(ConsoleColor TitleColor)
+        {
+            _titleColor = TitleColor;
+        }
+        public ConsoleCalendar()
+        {
+            _dayOfWeekColor = ConsoleColor.Cyan;
+            _dateColor = ConsoleColor.White;
+            _titleColor = ConsoleColor.Yellow;
+            _selectedDateColor = ConsoleColor.Blue;
+        }
+        public ConsoleCalendar(ConsoleColor TitleColor)
+        {
+            _dayOfWeekColor = TitleColor;
+            _dateColor = ConsoleColor.White;
+            _titleColor = ConsoleColor.Yellow;
+            _selectedDateColor = ConsoleColor.Blue;
+        }
+        public ConsoleCalendar(ConsoleColor TitleColor, ConsoleColor DateColor)
+        {
+            _dayOfWeekColor = TitleColor;
+            _dateColor = DateColor;
+            _titleColor = ConsoleColor.Yellow;
+            _selectedDateColor = ConsoleColor.Blue;
+        }
+        public ConsoleCalendar(ConsoleColor TitleColor, ConsoleColor DateColor, ConsoleColor SelectedDateColor)
+        {
+            _dayOfWeekColor = TitleColor;
+            _dateColor = DateColor;
+            _titleColor = ConsoleColor.Yellow;
+            _selectedDateColor = SelectedDateColor;
+        }
+        public ConsoleCalendar(ConsoleColor TitleColor, ConsoleColor DateColor, ConsoleColor SelectedDateColor, ConsoleColor DayOfWeekColor)
+        {
+            _dayOfWeekColor = TitleColor;
+            _dateColor = DateColor;
+            _titleColor = DayOfWeekColor;
+            _selectedDateColor = SelectedDateColor;
+        }
+        private void SetColor(int Counter, ref List<Int32> HitDays)
+        {
+            if (HitDays.Contains(Counter))
+            {
+                Console.ForegroundColor = _selectedDateColor;
+            }
+            else
+            {
+                Console.ForegroundColor = _dateColor;
+            }
+        }
+        private String GetSpaces(String MonthName)
+        {
+            String ReturnStr = String.Empty;
+            if (MonthName.Length <= 4)
+            {
+                ReturnStr = "     ";
+            }
+            else if (MonthName.Length <= 7)
+            {
+                ReturnStr = "    ";
+            }
+            else
+            {
+                ReturnStr = "   ";
+            }
+            return ReturnStr;
+        }
+        private String GetNextShortDay(String CurrentShortDay)
+        {
+            String ReturnStr = String.Empty;
+            switch (CurrentShortDay)
+            {
+                case "Mo":
+                    ReturnStr = "Tu";
+                    break;
+                case "Tu":
+                    ReturnStr = "We";
+                    break;
+                case "We":
+                    ReturnStr = "Th";
+                    break;
+                case "Th":
+                    ReturnStr = "Fr";
+                    break;
+                case "Fr":
+                    ReturnStr = "Sa";
+                    break;
+                case "Sa":
+                    ReturnStr = "Su";
+                    break;
+                case "Su":
+                    ReturnStr = "Mo";
+                    break;
+            }
+            return ReturnStr;
+        }
+        private String ConvertToShortDay(DayOfWeek CompareDay)
+        {
+            String ReturnStr = String.Empty;
+
+            switch (CompareDay)
+            {
+                case DayOfWeek.Monday:
+                    ReturnStr = "Mo";
+                    break;
+                case DayOfWeek.Tuesday:
+                    ReturnStr = "Tu";
+                    break;
+                case DayOfWeek.Wednesday:
+                    ReturnStr = "We";
+                    break;
+                case DayOfWeek.Thursday:
+                    ReturnStr = "Th";
+                    break;
+                case DayOfWeek.Friday:
+                    ReturnStr = "Fr";
+                    break;
+                case DayOfWeek.Saturday:
+                    ReturnStr = "Sa";
+                    break;
+                case DayOfWeek.Sunday:
+                    ReturnStr = "Su";
+                    break;
+            }
+            return ReturnStr;
+        }
+    }
+}
---
+++ ConsoleCalendarGeneratorConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿var CalendarExample1 = new CalendarGenerator.ConsoleCalendar();
+CalendarExample1.GenerateMonth(1, 2023, new List<DateTime>() { new DateTime(2023, 1, 1) });
+CalendarExample1.SetTitleColor(ConsoleColor.Red);
+CalendarExample1.GenerateMonth(2, 2023, new List<DateTime>() { new DateTime(2023, 2, 1), new DateTime(2023, 2, 4) });
+CalendarExample1.SetSelectedDateColor(ConsoleColor.Magenta);
+CalendarExample1.GenerateMonth(3, 2023, new List<DateTime>() { new DateTime(2023, 3, 1) });
+CalendarExample1.SetDayOfWeekColor(ConsoleColor.Green);
+CalendarExample1.GenerateMonth(4, 2023, new List<DateTime>() { new DateTime(2023, 4, 1) });
+CalendarExample1.SetDateColor(ConsoleColor.DarkYellow);
+CalendarExample1.GenerateMonth(5, 2023, new List<DateTime>() { new DateTime(2023, 5, 1), new DateTime(2023, 5, 8), new DateTime(2023, 5, 11) });
+Pause();
+
+var CalendarExample2 = new CalendarGenerator.ConsoleCalendar(ConsoleColor.Red);
+List<DateTime> HitDates = new List<DateTime>()
+{
+    new DateTime(2023, 1, 1),
+    new DateTime(2023, 2, 1), 
+    new DateTime(2023, 2, 4),
+    new DateTime(2023, 3, 1),
+    new DateTime(2023, 4, 1),
+    new DateTime(2023, 5, 1), 
+    new DateTime(2023, 5, 8), 
+    new DateTime(2023, 5, 11)
+};
+CalendarExample2.GenerateMonth(1, 2023, HitDates);
+CalendarExample2.GenerateMonth(2, 2023, HitDates);
+CalendarExample2.GenerateMonth(3, 2023, HitDates);
+CalendarExample2.GenerateMonth(4, 2023, HitDates);
+CalendarExample2.GenerateMonth(5, 2023, HitDates);
+Pause();
+
+CalendarExample2.SetTitleColor(ConsoleColor.DarkMagenta);
+CalendarExample2.GenerateYear(2023);
+Pause();
+
+var CalendarExample4 = new CalendarGenerator.ConsoleCalendar(ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Magenta, ConsoleColor.Blue);
+CalendarExample4.GenerateYear(2023, HitDates);
+Console.WriteLine("\n\nPress any key to terminate...");
+Console.ReadKey();
+void Pause()
+{
+    Console.WriteLine("\n\nPress any key for next example...");
+    Console.ReadKey();
+    Console.Write("\f\u001bc\x1b[3J");
+}
